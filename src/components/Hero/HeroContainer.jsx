2a2bd54c89891513a647f1e9c96933d3c74a97db
Nurbek Beisheev <beibus@Nurbeks-MacBook-Pro.local>
2022-10-15 18:13:25 +0600

    header and hero container

---
+++ src/components/Hero/HeroContainer.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import image from '../../image/hero-image.png'
+import './HeroContainer.css'
+
+const HeroContainer = () => {
+  return (
+    <div className='hero-container'>
+      <div className="hero-content">
+        <div className="hero-text">
+          <h2 className="hero-title">Gift.me</h2>
+          <h3 className='title-text'>Мы воплощаем в подарках вашу любовь!</h3>
+        </div>
+
+        <div className="hero-buttons">
+          <div className='button-enter'>
+          <button className='enter'>Вход</button>
+          </div>
+
+          <div className='button-login'>
+          <button>Регистрация</button>
+          </div>
+        </div>
+
+      </div>
+
+      <div className="hero-image">
+        <img src={image} alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default HeroContainer
